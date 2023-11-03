8bd4104de9a89099415fa0678fb87d122c4c0015
Brian Hicks <brian@brianthicks.com>
2023-11-03 14:41:11 -0500

    nixify

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import inputs.nixpkgs { inherit system; };
+      in {
+        formatter = pkgs.nixpkgs-fmt;
+
+        devShell = pkgs.mkShell {
+          packages = [
+            pkgs.elmPackages.elm
+            pkgs.elmPackages.elm-format
+            pkgs.elmPackages.elm-test
+            pkgs.elmPackages.elm-language-server
+            pkgs.elmPackages.elm-json
+          ];
+        };
+      }
+    );
+}
